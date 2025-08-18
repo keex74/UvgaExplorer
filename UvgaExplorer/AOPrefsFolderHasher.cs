4e4465b3af0e97e771c86fe50c5bbb34ab668be3
Keex <keex1010110@gmail.com>
2025-08-18 21:01:02 +0200

    Added function to generate full prefs folder name.

--- UvgaExplorer/AOPrefsFolderHasher.cs
+++ UvgaExplorer/AOPrefsFolderHasher.cs
@@ -16,11 +16,12 @@ public static class AOPrefsFolderHasher
     /// </summary>
     /// <param name="aoGameFolder">The AO game directory, where e.g. AnarchyOnline.exe lives.</param>
     /// <returns>The hash string used in %localappdata\Funcom\Anarchy Online\-hash-\...</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="aoGameFolder"/> is null or empty.</exception>
     public static string GetPrefsHash(string aoGameFolder)
     {
         if (string.IsNullOrEmpty(aoGameFolder))
         {
-            return string.Empty;
+            throw new ArgumentNullException(nameof(aoGameFolder));
         }
 
         // Find parent folder and convert path to use forward slashes
@@ -32,6 +33,14 @@ public static class AOPrefsFolderHasher
         }
 
         var parent = parentFolder.FullName;
+        if (parent.EndsWith(Path.DirectorySeparatorChar))
+        {
+            // Cut of the directory separator at the end.
+            // It shouldn't be there in the first place, but I don't think that's guaranteed.
+            parent = parent[..^1];
+        }
+
+        // Boost library TPATH uses slashes for directory separators
         parent = parent.Replace(Path.DirectorySeparatorChar, '/');
 
         // Do the actual hash calculation, from Boost 1.53 library boost\functional\hash.hpp
@@ -49,5 +58,27 @@ public static class AOPrefsFolderHasher
         Array.Reverse(hashBytes);
         var hashStr = BitConverter.ToString(hashBytes).ToLower().Replace("-", string.Empty);
         return hashStr;
+    }
+
+    /// <summary>
+    /// Calculate the AO preference folder hash from the given AO game directory.
+    /// </summary>
+    /// <param name="aoGameFolder">The AO game directory, where e.g. AnarchyOnline.exe lives.</param>
+    /// <returns>The hash string used in %localappdata\Funcom\Anarchy Online\-hash-\...</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="aoGameFolder"/> is null or empty.</exception>
+    public static DirectoryInfo GetPrefsFolder(string aoGameFolder)
+    {
+        if (string.IsNullOrEmpty(aoGameFolder))
+        {
+            throw new ArgumentNullException(nameof(aoGameFolder));
+        }
+
+        // Find parent folder and convert path to use forward slashes
+        aoGameFolder = Path.GetFullPath(aoGameFolder);
+        var aoGameFolderName = Path.GetFileName(aoGameFolder);
+        var localAppdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var hash = GetPrefsHash(aoGameFolder);
+        var prefsPath = Path.Combine(localAppdata, "Funcom", "Anarchy Online", hash, aoGameFolderName);
+        return new DirectoryInfo(prefsPath);
     }
 }
